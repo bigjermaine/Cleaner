539397895a3ca2c47631314dfea52af68a4b1691
jermaine-iris <danieljermaine97@gmail.com>
2025-07-05 21:10:08 +0100

    Initial Commit

---
+++ Cleaner/CleanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanerApp.swift
+//  Cleaner
+//
+//  Created by Daniel Jermaine on 05/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct CleanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
